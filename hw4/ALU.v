8564c8b050a0be53c6ce7a631dd14217c8eb013b
jasonhojh1122 <jasonhojh1122@gmail.com>
2020-11-22 13:10:45 +0800

    change input and result to signed number

--- hw4/ALU.v
+++ hw4/ALU.v
@@ -7,14 +7,14 @@ module ALU
     Zero_o
 );
 
-input   [31:0]      data1_i;
-input   [31:0]      data2_i;
-input   [2:0]       ALUCtrl_i;
-output  [31:0]      data_o;
-output              Zero_o;
+input signed    [31:0]      data1_i;
+input           [31:0]      data2_i;
+input           [2:0]       ALUCtrl_i;
+output          [31:0]      data_o;
+output                      Zero_o;
 
-reg [31:0]  result;
-reg         Zero_reg;
+reg signed [31:0]   result;
+reg                 Zero_reg;
 
 assign data_o = result;
 assign Zero_o = Zero_reg;
